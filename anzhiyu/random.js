f16595c6a5ed22da4cdc8b8b9eafd824c69ceb58
mkmkgo <wy950110@gmail.com>
2025-07-31 12:03:29 +0000

    Deploy via GitHub Actions: 1b1582598f3a4d548cfa65fe38bb36d598c8a951

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3eeb/","posts/3eeb/","posts/3eeb/"];function toRandomPost(){
+var posts=["posts/3eeb/","posts/3eeb/","posts/3eeb/","posts/54d0/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

4c469ef9f683aaa7123b76d09a68b1c2ee220000
mkmkgo <wy950110@gmail.com>
2025-07-31 03:04:31 +0000

    Deploy via GitHub Actions: 707cc93425d0beab45d6eca763146a8455bec001

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/30/hello-world/","2025/07/30/hello-world_副本/","2025/07/30/hello-world_副本2/"];function toRandomPost(){
+var posts=["2025/07/31/hello-world/","2025/07/31/hello-world_副本/","2025/07/31/hello-world_副本2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
